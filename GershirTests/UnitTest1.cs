4aa29a7c5d8c2628bfac376d5b6637bed2f1f43c
VKalinin <v.kalinin.m94@gmail.com>
2022-06-19 17:00:19 +0300

    Sleep changes

--- GershirTests/UnitTest1.cs
+++ GershirTests/UnitTest1.cs
@@ -9,10 +9,10 @@ namespace GershirTests
         private const string _firstName = "Vladyslav";
         private const string _phoneNumber = "0992132687";
         private const string _textAreaComment = "Тест успешно пройден";
+        private const int _clicksN = 5;
+        private const int _sleepN = 400;
 
-
-        //private readonly By _englishLanguageButton = By.XPath("//a[@title='English']");
-        //private readonly By _menuButton = By.XPath("//a[@href='https://dostavka.gershir.com.ua/menu/']/span[@class='nav-link-text']");
+        
         private readonly By _gershirBeerButton = By.XPath("//span[text()='Пиво «Гершир»']");
         private readonly By _lightBeerButton = By.XPath("/html/body/div[1]/div[1]/div[2]/div/div/div[4]/div[2]/div/div[3]/h3/a");
         private readonly By _addToCardButton = By.XPath("//button[@name='add-to-cart']");
@@ -41,17 +41,18 @@ namespace GershirTests
             lightBeer.Click();
             Thread.Sleep(400);
             var plusButton = driver.FindElement(_plusButton);
-            plusButton.Click();
-            Thread.Sleep(400);
-            plusButton.Click();
-            Thread.Sleep(400);
-            plusButton.Click();
-            Thread.Sleep(400);
-            plusButton.Click();
-            Thread.Sleep(400);
+            var doMultipleClicks = (_clicksN, _sleepN);
+            {
+                for(int i = 0; i < _clicksN; i++)
+                {
+                    plusButton.Click();
+                    Thread.Sleep(_sleepN);
+
+                }
+            }
             var addToCard = driver.FindElement(_addToCardButton);
             addToCard.Click();
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
             var checkOut = driver.FindElement(_checkOutButton);
             checkOut.Click();
             var firstNameField = driver.FindElement(_firstNameField);
@@ -60,7 +61,7 @@ namespace GershirTests
             phoneNumberField.SendKeys(_phoneNumber);
             var textAreaCommentField = driver.FindElement(_textAreaCommentField);
             textAreaCommentField.SendKeys(_textAreaComment);
-            Thread.Sleep(10000);
+            Thread.Sleep(5000);
 
         }
 
